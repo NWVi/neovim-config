385f33d64a424c90b5820cdcd84f1039ed2aa1d0
Sebastian Olsen <seppen.o@gmail.com>
2021-10-29 03:22:14 +0200

    Move settings to own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,3 @@
-
-require('nwvi.util')
-require('nwvi.plugins')
-
-require("indent_blankline").setup {
-    char = "|",
-    buftype_exclude = {"terminal"}
-}
-
-
-require('nvim-web-devicons').setup() -- ensable devicons
-
-require('colorizer').setup() -- enable color higlighting
-
 --- Helpers ---
 local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
@@ -24,24 +10,8 @@ local function map(mode, keys, actions, opts)
   vim.api.nvim_set_keymap(mode, keys, actions, options)
 end
 
---- Options  ---
-opt.mouse = 'a' -- enable mouse mode
-opt.tabstop = 2 -- default spaces for tab
-opt.shiftwidth = 2 -- size of an indent
-opt.shiftround = true -- round indent to multiple of shiftwidth
-opt.expandtab = true -- use spaces instead of tabs
-opt.number = true -- show numbers
-opt.ignorecase = true -- Case insensitive
-opt.smartcase = true -- Case insensitive unless /C or capital in search
-opt.breakindent = true -- Enable break indent
-opt.smartindent = true -- Smart autoindenting on new line
-opt.updatetime = 250 -- milliseconds from typing to saving swap file
-opt.list = true -- show some invisible characters
-opt.termguicolors=true -- True color support
-opt.hidden = true -- Do not save when switching buffers
-
 --- Keybindings ---
-vim.g.mapleader = ' ' -- Set SPACE as leader key
+g.mapleader = ' ' -- Set SPACE as leader key
 map('', '<leader>c', '"+y') -- Copy to system clipboard
 map('n', '<leader>w', ':w<CR>') -- Save
 map('n', '<leader>q', ':q<CR>') -- Quit current window
@@ -52,15 +22,25 @@ map('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previ
 map('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
 map('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
 map('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
-map('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+map('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+map('n', '<leader>sl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 map('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
 map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
-vim.g.tmux_navigator_no_mappings = true
+g.tmux_navigator_no_mappings = true
 map('n', '<M-h>', ':TmuxNavigateLeft<CR>', { silent = true } ) -- Navigate left (buffer and tmux)
 map('n', '<M-j>', ':TmuxNavigateDown<CR>', { silent = true }) -- Navigate down (buffer and tmux)
 map('n', '<M-k>', ':TmuxNavigateUp<CR>', { silent = true }) -- Navigate up (buffer and tmux)
 map('n', '<M-l>', ':TmuxNavigateRight<CR>', { silent = true }) -- Navigate right (buffer and tmux)
 map('n', '<C-n>', ':bnext<CR>') -- Next buffer
 map('n', '<C-p>', ':bprev<CR>') -- Previous buffer
+
+map('n', '<Esc>', ':nohlsearch<Bar>:echo<CR>', { silent = true }) -- Disable highlight and clear any message displayed
+
+require('nwvi.util')
+require('nwvi.settings')
+require('nwvi.plugins')
+
+require('nvim-web-devicons').setup() -- ensable devicons
+
+require('colorizer').setup() -- enable color higlighting
---
+++ nvim/lua/nwvi/settings.lua
@@ -0,0 +1,27 @@
+--- Options  ---
+vim.opt.mouse = 'a' -- enable mouse mode
+
+vim.opt.tabstop = 2 -- default spaces for tab
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- size of an indent
+vim.opt.shiftround = true -- round indent to multiple of shiftwidth
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.smartindent = true -- Smart autoindenting on new line
+
+vim.opt.number = true -- show numbers
+vim.opt.ignorecase = true -- Case insensitive
+vim.opt.smartcase = true -- Case insensitive unless /C or capital in search
+vim.opt.updatetime = 250 -- milliseconds from typing to saving swap file
+vim.opt.termguicolors=true -- True color support
+vim.opt.hidden = true -- Do not save when switching buffers
+
+-- Display options for hidden charactrs --
+vim.opt.list = true -- show some invisible characters
+vim.opt.listchars = {
+    space = "⋅",
+    tab = "__",
+    trail = "•",
+    extends = "❯",
+    precedes = "❮",
+    nbsp = "_",
+}
