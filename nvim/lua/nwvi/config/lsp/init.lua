f638d7ccc6f43264d9503622db64e16488401431
Sebastian Olsen <seppen.o@gmail.com>
2021-11-05 14:03:28 +0100

    Fix angularls

--- nvim/lua/nwvi/config/lsp/init.lua
+++ nvim/lua/nwvi/config/lsp/init.lua
@@ -3,10 +3,19 @@ return function()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   local coq = require('coq')
 
-  local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'angularls' }
+  local nglsPath = vim.fn.stdpath('data')..'/lsp_servers/angularls/node_modules'
+  local tsserverPath = vim.fn.stdpath('data')..'/lsp_servers/tsserver/node_modules'
+  local angular_cmd = {"ngserver", "--stdio", "--ngProbeLocations", nglsPath, "--tsProbeLocations", tsserverPath }
 
   lsp_installer.on_server_ready(function(server)
     local opts = {}
+
+    if server.name == "angularls" then
+      opts.cmd = angular_cmd
+      opts.on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = angular_cmd
+      end
+    end
 
     server:setup(coq.lsp_ensure_capabilities(opts))
   end)
