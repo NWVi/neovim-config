571cdd5ac3443a542330ca56195ffa12727d5c87
Sebastian Olsen <seppen.o@gmail.com>
2021-11-12 15:04:18 +0100

    Move telescope keybinds to telescope config

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,15 +6,6 @@ map('n', '<leader>w', ':w<CR>') -- Save
 map('n', '<leader>q', ':q<CR>') -- Quit current window
 map('n', '<leader>qa', ':qa<CR>') -- Quit all windows
 map('n', '<leader>r', [[<cmd>lua require('nwvi.util.helpers').reload_config()<CR>]]) -- Reload config files
-map('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
-map('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]])
-map('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]])
-map('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]])
-map('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]])
-map('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]])
-map('n', '<leader>sl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]])
-map('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]])
-map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]])
 map('n', '<Esc>', ':nohlsearch<Bar>:echo<CR>', { silent = true }) -- Disable highlight and clear any message displayed
 
 --- TMUX ---
--- nvim/lua/nwvi/config/telescope.lua
+++ nvim/lua/nwvi/config/telescope.lua
@@ -7,11 +7,34 @@ return function()
   local actions = require 'telescope.actions'
   telescope.setup({
     defaults = {
+      vimgrep_arguments = {
+        'rg',
+        '--smart-case',
+      },
       mappings = {
         i = {
           ['<esc>'] = actions.close,
         },
       },
     },
+    pickers = {
+      find_files = {
+        find_command = {
+          'rg', '--files', '--hidden', '--glob', '!.git/*'
+        }
+      }
+    }
   })
+
+
+  local map = require('nwvi.util.helpers').keymap.map
+  map('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
+  map('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
+  map('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]])
+  map('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]])
+  map('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]])
+  map('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]])
+  map('n', '<leader>sl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]])
+  map('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]])
+  map('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]])
 end
