2094f9ef2d94483358949ace6e164e6d4123d48f
Sebastian Olsen <seppen.o@gmail.com>
2022-04-20 13:00:10 +0200

    search and cmdline completion

--- nvim/lua/nwvi/config/cmp.lua
+++ nvim/lua/nwvi/config/cmp.lua
@@ -46,4 +46,21 @@ return function()
       { name = 'luasnip' },
     },
   })
+
+  -- Use buffer source for `/`
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' },
+    },
+  })
+
+  -- Use cmdline & path source for ':'
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'cmdline' },
+      { name = 'path' },
+    },
+  })
 end
