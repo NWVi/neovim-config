5478c09bc9c79e7e048518bc885c6d7f8ca9293e
Sebastian Olsen <seppen.o@gmail.com>
2022-05-10 14:07:03 +0200

    fix lsp keymaps

--- nvim/lua/nwvi/config/lsp/keys.lua
+++ nvim/lua/nwvi/config/lsp/keys.lua
@@ -2,29 +2,29 @@ local M = {}
 
 function M.setup(bufnr)
   local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', 'vim.lsp.buf.declaration()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', 'vim.lsp.buf.definition()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', 'vim.lsp.buf.hover()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', 'vim.lsp.buf.implementation()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', 'vim.lsp.buf.signature_help()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lwa', 'vim.lsp.buf.add_workspace_folder()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lwr', 'vim.lsp.buf.remove_workspace_folder()', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lwa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lwr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(
     bufnr,
     'n',
     '<space>lwl',
-    'print(vim.inspect(vim.lsp.buf.list_workspace_folders()))',
+    '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))',
     opts
   )
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lD', 'vim.lsp.buf.type_definition()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lr', 'vim.lsp.buf.rename()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<A-CR>', 'vim.lsp.buf.code_action()', opts)
-  -- lua_buf_map('n', 'gr', 'vim.lsp.buf.references()') -- Handled by trouble.nvim
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>le', 'vim.lsp.diagnostic.show_line_diagnostics()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', 'vim.lsp.diagnostic.goto_prev({border = "rounded"})', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', 'vim.lsp.diagnostic.goto_next({border = "rounded"})', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lq', 'vim.lsp.diagnostic.set_loclist()', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', 'vim.lsp.buf.formatting()', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lD', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<A-CR>', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- lua_buf_map('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>') -- Handled by trouble.nvim
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>le', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev({border = "rounded"})<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next({border = "rounded"})<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>lq', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
   vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
