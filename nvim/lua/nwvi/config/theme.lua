f9c717c1c36daaf795d6b44a2efc18a6d39efad7
Sebastian Olsen <seppen.o@gmail.com>
2022-06-15 14:40:36 +0200

    Update kanagawa theme config

--- nvim/lua/nwvi/config/theme.lua
+++ nvim/lua/nwvi/config/theme.lua
@@ -1,12 +1,12 @@
 return function()
   require('kanagawa').setup({
     undercurl = true, -- enable undercurls
-    commentStyle = 'italic',
-    functionStyle = 'NONE',
-    keywordStyle = 'italic',
-    statementStyle = 'bold',
-    typeStyle = 'NONE',
-    variablebuiltinStyle = 'italic',
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = true },
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
     transparent = false, -- do not set background color
@@ -14,9 +14,9 @@ return function()
     globalStatus = true, -- adjust window separators highlight for laststatus=3
     colors = {},
     overrides = {
-      IndentBlanklineChar = { style = 'nocombine' },
-      IndentBlanklineSpaceChar = { style = 'nocombine' },
-      IndentBlanklineSpaceCharBlankline = { style = 'nocombine' },
+      IndentBlanklineChar = { nocombine = true },
+      IndentBlanklineSpaceChar = { nocombine = true },
+      IndentBlanklineSpaceCharBlankline = { nocombine = true },
       BufferCurrent = { bg = '#363646' },
       BufferCurrentIndex = { bg = '#363646' },
       BufferCurrentMod = { bg = '#363646', fg = '#FF9E3B' },
