7087da11c1f3c61fad90aca5ef563fa8850a6296
Sebastian Olsen <seppen.o@gmail.com>
2022-08-07 19:57:19 +0200

    Allow pane naviagation with ctrl+arrows

--- nvim/lua/nwvi/config/navigate.lua
+++ nvim/lua/nwvi/config/navigate.lua
@@ -8,8 +8,8 @@ return function()
     vim.g.tmux_navigator_no_mappings = true
   end
 
-  map('n', '<M-h>', ':' .. navTool .. 'NavigateLeft<CR>') -- Navigate left (buffer and tmux)
-  map('n', '<M-j>', ':' .. navTool .. 'NavigateDown<CR>') -- Navigate down (buffer and tmux)
-  map('n', '<M-k>', ':' .. navTool .. 'NavigateUp<CR>') -- Navigate up (buffer and tmux)
-  map('n', '<M-l>', ':' .. navTool .. 'NavigateRight<CR>') -- Navigate right (buffer and tmux)
+  map('n', '<C-Left>', ':' .. navTool .. 'NavigateLeft<CR>') -- Navigate left (buffer and tmux)
+  map('n', '<C-Down>', ':' .. navTool .. 'NavigateDown<CR>') -- Navigate down (buffer and tmux)
+  map('n', '<C-Up>', ':' .. navTool .. 'NavigateUp<CR>') -- Navigate up (buffer and tmux)
+  map('n', '<C-Right>', ':' .. navTool .. 'NavigateRight<CR>') -- Navigate right (buffer and tmux)
 end
