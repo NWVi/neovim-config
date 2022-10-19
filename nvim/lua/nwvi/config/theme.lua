4b700f237e70b6c6ffe097711e859901d3681a21
Sebastian Olsen <seppen.o@gmail.com>
2022-10-19 14:24:10 +0200

    Dim inactive window

--- nvim/lua/nwvi/config/theme.lua
+++ nvim/lua/nwvi/config/theme.lua
@@ -10,7 +10,7 @@ return function()
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
     transparent = false, -- do not set background color
-    -- dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+    dimInactive = true, -- dim inactive window `:h hl-NormalNC`
     globalStatus = true, -- adjust window separators highlight for laststatus=3
     colors = {},
     overrides = {
